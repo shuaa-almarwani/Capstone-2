e08b43506842668169fe94d736d3007c1a41aa7a
SHUAA <shuaa.almarwani@gmail.com>
2026-05-13 20:56:22 +0300

    Project Setup

---
+++ src/test/java/com/example/cloud_kitchen/CloudKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud_kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
